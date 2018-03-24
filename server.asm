6cac4aab14a35d8a65b8656aa763f897a5deaf27
HBchevelle68 <aportante10@gmail.com>
2018-03-24 16:37:06 -0700

    read/write functions

--- server.asm
+++ server.asm
@@ -23,32 +23,40 @@ _start:
   nop
   nop
 
+  ;; create socket
   call    csocket
   test    ax, ax
   js      .err
   mov     [sfd], ax
 
+  ;; set socket opts
   mov     rdi, [sfd]
   call    csetsockopt
   test    ax, ax
   js     .err
 
+  ;; bind to addr space
   mov    rdi, [sfd]
   mov    rsi, 0xE110
   call   cbind
   test   ax, ax
   js     .err
 
+  ;; begin listen
   mov    rdi, [sfd]
   call   clisten
   test   ax, ax
   js     .err
 
+  ;; accept connections
   mov    rdi, [sfd]
   call   caccept
   test   ax, ax
   js     .err
 
+  
+
+  ;; temp error label/exit
 .err:
   mov    dil, al
   call   exit
