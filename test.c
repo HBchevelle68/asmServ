e0c39572851fc27e15b69c1bd1d7acbe742d59a5
HBchevelle68 <aportante10@gmail.com>
2018-04-02 21:50:27 -0400

    Update test.c

--- test.c
+++ test.c
@@ -9,11 +9,5 @@ int main(){
 
   printf("%lu\n", sizeof(struct stat) );
 
-  int array[2];
-  array[0] = foo(2);
-  for (size_t i = 0; i < 2; i++) {
-    printf("%d\n", array[i]);
-  }
-
   return 0;
 }
